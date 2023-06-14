5ccd0d458e0c497244f5844b16a7622e8cd19ba6
AbdElwahap-Bakdones <114876536+AbdElwahap-Bakdones@users.noreply.github.com>
2023-06-14 22:36:40 +0300

    Update settings.py

--- motors_more/motors_more/settings.py
+++ motors_more/motors_more/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z#q000g=*o255f=yloect*efe$wplbhrhq-p+ctd!w2rci=szq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['abdelwahapbak2.pythonanywhere.com']
 
 
 # Application definition

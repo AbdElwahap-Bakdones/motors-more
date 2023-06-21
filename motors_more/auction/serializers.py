e25ab335e91b93611af46db38f2ca94e3056827d
AbdElwahap-Bakdones <114876536+AbdElwahap-Bakdones@users.noreply.github.com>
2023-06-21 18:16:29 +0300

    add CarSerializer

--- motors_more/auction/serializers.py
+++ motors_more/auction/serializers.py
@@ -9,8 +9,24 @@ class UserCreateSerializer(DjoserUserCreateSerializer):
 
 
 class ClientCreateSerializer(serializers.ModelSerializer):
-    user_id = UserCreateSerializer
-
+    user = UserCreateSerializer(source='user_id',read_only=True)
     class Meta:
         model = models.Client
-        fields = ['id', 'user_id', 'gender']
+        fields = ['id', 'user', 'gender']
+
+    # def create(self, validated_data):
+    #     print(validated_data)
+    #     user_data = validated_data.pop('user_id')
+    #     user = models.User.objects.create(**user_data)
+    #     client = models.Client.objects.create(user_id=user, **validated_data)
+        # return client
+    
+class CarSerializer(serializers.ModelSerializer):
+    user = UserCreateSerializer(source='user_id',read_only=True)
+    manufacturing_year_ = serializers.DateField(source ='manufacturing_year',format='%Y-%m-%d')
+
+    class Meta:
+        model = models.Car
+        fields = ['user_id','user','mileage','color','type','manufacturing_year_',
+                  'clean_title','engine_type','gear_type','cylinders','notes','price','location']
+        

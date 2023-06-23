eb0d86f6d317a5f77f722ef271e4661372d0c1be
AbdElwahap-Bakdones <114876536+AbdElwahap-Bakdones@users.noreply.github.com>
2023-06-23 22:17:44 +0300

    edit

--- motors_more/auction/views.py
+++ motors_more/auction/views.py
@@ -50,18 +50,18 @@ class Cars(generics.ListCreateAPIView):
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
     def create(self, request, *args, **kwargs):
-        try:
-            # request.data['user_id']=request.user.pk
-            # request.data['user_id']='ee@gg.com'
-            print(request.data)
-            serializer = self.get_serializer(data=request.data)
-            serializer.is_valid(raise_exception=True)
-            self.perform_create(serializer)
-            headers = self.get_success_headers(serializer.data)
-            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-        except Exception as e:
-            print(f'Error in Cars.create  {str(e)}')
-            return Response({'message':'server Error'},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
+        # request.data['user_id']=request.user.pk
+        # request.data['user_id']='ee@gg.com'
+        print(request.data)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        # except Exception as e:
+        #     print(f'Error in Cars.create  {str(e)}')
+        #     return Response({'message':'server Error'},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 class Car(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.Car.objects.all()
     serializer_class = serializers.CarSerializer

d32e3d54ec5e104ab36c389d31521b856404dbe1
AbdElwahap-Bakdones <114876536+AbdElwahap-Bakdones@users.noreply.github.com>
2023-07-31 02:49:11 +0300

    commit

--- motors_more/auction/schedule.py
+++ motors_more/auction/schedule.py
@@ -1,6 +1,24 @@
-import datetime
+from datetime import datetime
+import time
+import threading
+
+global f
 
 
 def print_something():
-    print("2313131232131")
-    print(datetime.time)
+    print('cron ***************is**************** run')
+    f = 0
+    while True:
+        time.sleep(5)
+        f = f+1
+        print("2313131232131")
+        print(datetime.now())
+        if f > 2:
+            print_something()
+
+
+def run_cron_job():
+    print('cron ***************is**************** runing')
+    job_thread = threading.Thread(target=print_something)
+    job_thread.daemon = True
+    job_thread.start()
